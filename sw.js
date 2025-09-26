ff3b49b8c1309451573a0003de513ea2b34838bb
yr2610 <styr21182610@gmail.com>
2025-09-27 03:21:26 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'qrscanner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+  // オフラインで使いたいライブラリをローカル同梱した場合はここに追加
+  // 例: './libs/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  }
+});
